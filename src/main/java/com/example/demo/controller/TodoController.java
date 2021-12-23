0c991fceb51e061b5d0426708a21d21189e772f2
yh.thomas.lee <yh.thomas.lee@gmail.com>
2021-12-23 21:12:23 +0900

    dec 23rd work... TodoService and TodoController were added

---
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ResponseDTO;
+
+import service.TodoService;
+// 실습코드 2-21. 12.23.2021
+@RestController
+@RequestMapping("todo")
+public class TodoController {
+	//TestTodo 메서드 작성하기 
+	//실습코드 2-22. TodoController에서 TodoService사용
+	@Autowired
+	private TodoService service;
+	
+	@GetMapping("/test")
+	public ResponseEntity<?> testTodo() {
+		String str = service.testService(); //테스트 서비스 사용
+		List<String> list =new ArrayList<>();
+		list.add(str);
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
+	}
+}
---
+++ src/main/java/service/TodoService.java
@@ -0,0 +1,11 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+	public String testService() {
+		return "Test Service";
+	}
+}
